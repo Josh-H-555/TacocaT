45ab9685a00b7b46de29a22274bcc3f3ef15b480
Josh Hutchinson <64669694+Josh-H-555@users.noreply.github.com>
2022-04-11 23:16:14 -0700

    Initial Commit
    Completed Project!

--- js/site.js
+++ js/site.js
@@ -1,4 +1,78 @@
-//Call Hello World
-function HelloWorld() {
-    alert("Hello world!");
+//Instantiate event listener
+document.getElementById("buttonSubmit").addEventListener("click", getValues);
+
+document.getElementById("alert").classList.add("invisible");
+
+//getValues
+function getValues() {
+
+
+    let inputString = document.getElementById("userInput").value;
+
+    let palindrome = false;
+
+    if (inputString) {
+        isolatedString = inputString.replace(/[^a-zA-Z\\s]/gi, '').toLowerCase();
+        isolatedString = isolatedString.replace(/\\/gi, '');
+        palindrome = checkPalindrome(isolatedString);
+        displayPalindrome(inputString, isolatedString, palindrome);
+    } else {
+        alert("Please enter a string.");
+    }
+}
+
+//checkPalindrome
+function checkPalindrome(isolatedString) {
+
+    let stringLength = isolatedString.length;
+
+    for (let i = 0; i < Math.floor(stringLength / 2); ++i) {
+        if (isolatedString[i] != isolatedString[stringLength - i - 1]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function checkPalindrome(isolatedString, poop) {
+
+    let stringLength = isolatedString.length;
+
+    for (let i = 0; i < Math.floor(stringLength / 2); ++i) {
+        if (isolatedString[i] != isolatedString[stringLength - i - 1]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function checkPalindrome() {
+
+    let stringLength = isolatedString.length;
+
+    for (let i = 0; i < Math.floor(stringLength / 2); ++i) {
+        if (isolatedString[i] != isolatedString[stringLength - i - 1]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+//displayPalindrome
+function displayPalindrome(inputString, isolatedString, palindrome) {
+    message = document.getElementById("message");
+    alertBox = document.getElementById("alert");
+
+    if (palindrome) {
+        message.innerHTML = `The string you entered "${isolatedString}" is a palindrome!`;
+        alertBox.classList.add("alert-success");
+        alertBox.classList.remove("invisible");
+    } else {
+        message.innerHTML = `The string you entered "${isolatedString}" is not a palindrome.`;
+        alertBox.classList.add("alert-danger");
+        alertBox.classList.remove("invisible");
+    }
 }
